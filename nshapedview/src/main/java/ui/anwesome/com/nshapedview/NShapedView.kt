8a1a08c919e09083ba500b4c8960026fdb89d0a2
Anwesh43 <anweshthecool0@gmail.com>
2018-02-15 08:06:43 +0530

    adding Renderer in View

--- nshapedview/src/main/java/ui/anwesome/com/nshapedview/NShapedView.kt
+++ nshapedview/src/main/java/ui/anwesome/com/nshapedview/NShapedView.kt
@@ -9,14 +9,13 @@ import android.graphics.*
 
 class NShapedView(ctx:Context):View(ctx) {
     val paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    val renderer = NShapeRenderer(this)
     override fun onDraw(canvas:Canvas) {
-
+        renderer.render(canvas,paint)
     }
     override fun onTouchEvent(event:MotionEvent):Boolean {
         when(event.action) {
-            MotionEvent.ACTION_DOWN -> {
-
-            }
+            MotionEvent.ACTION_DOWN -> renderer.handleTap()
         }
         return true
     }
