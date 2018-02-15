f87e4d2adb7a1642fab286c31156e285bd893a14
Anwesh43 <anweshthecool0@gmail.com>
2018-02-15 08:43:31 +0530

    adding fullScreen support in Activity

--- app/src/main/java/ui/anwesome/com/kotlinnshapedview/MainActivity.kt
+++ app/src/main/java/ui/anwesome/com/kotlinnshapedview/MainActivity.kt
@@ -2,11 +2,18 @@ package ui.anwesome.com.kotlinnshapedview
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowManager
+import ui.anwesome.com.nshapedview.NShapedView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
+        NShapedView.create(this)
+        fullScreen()
     }
+}
+fun MainActivity.fullScreen() {
+    supportActionBar?.hide()
+    window.addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN)
 }
